1acc4785931ed050086afc0bc5ac439386dd91f1
eltonsa75 <eltonsa75@hotmail.com>
2024-09-11 16:40:55 -0300

    Serviço de Contador (count) de Ciclo de Pagamento

--- CursoFramesWeb/backend/api/BillingCycle/billingCycleService.js
+++ CursoFramesWeb/backend/api/BillingCycle/billingCycleService.js
@@ -1,7 +1,18 @@
+const { functionsIn } = require('lodash')
 const billingCycle = require('./billingCycle')
 const BillingCycle = require('./billingCycle')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 billingCycle.updateOptions({new: true, runValidators: true})
+
+BillingCycleillingCycle.route('count', function(req, res, next ){
+    BillingCycle.count(function(error, value) {
+        if(error) {
+            res.status(500).json({errors: [error]})
+        } else {
+            res.json({value})
+        }
+    })
+})
 
 module.exports = BillingCycle

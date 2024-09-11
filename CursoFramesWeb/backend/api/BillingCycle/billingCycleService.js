37f25c13822ad6e2e7e1978b8c86a639f8765ec9
eltonsa75 <eltonsa75@hotmail.com>
2024-09-11 16:31:27 -0300

    Criando msg de validação no Mongo

--- CursoFramesWeb/backend/api/BillingCycle/billingCycleService.js
+++ CursoFramesWeb/backend/api/BillingCycle/billingCycleService.js
@@ -1,5 +1,7 @@
+const billingCycle = require('./billingCycle')
 const BillingCycle = require('./billingCycle')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
+billingCycle.updateOptions({new: true, runValidators: true})
 
 module.exports = BillingCycle
--- CursoFramesWeb/backend/config/database.js
+++ CursoFramesWeb/backend/config/database.js
@@ -5,7 +5,9 @@ mongoose.connect('mongodb://localhost/db_finance')
   .catch(err => console.log('MongoDB connection error:', err));
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
-
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite minimo de '{MIN}'."
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite máximo de '{MAX}'."
+mongoose.Error.messages.String.enum = "O '{VALUE}' não é válido para o atributo '{PATH}'."
 
 
 

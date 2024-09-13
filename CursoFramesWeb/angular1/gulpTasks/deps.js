3b60a9b752f6205a629b9f45fde13d37fc77e811
eltonsa75 <eltonsa75@hotmail.com>
2024-09-13 11:31:26 -0300

    Criando as Dependencias do Gulp

--- CursoFramesWeb/angular1/gulpTasks/deps.js
+++ CursoFramesWeb/angular1/gulpTasks/deps.js
@@ -10,13 +10,38 @@ gulp.task('deps', ['deps.js', 'deps.css', 'deps.fonts'])
 
 gulp.task('deps.js', () => {
     return gulp.src([
-        '...'
+        'node_modules/angular/angular.min.js',
+        'node_modules/angular-ui-router/release/angular-ui-router.min.js',
+        'node_modules/angular-animate/angular-animate.min.js',
+        'node_modules/angular-toastr/dist/angular-toastr.tpls.min.js',
+        'node_modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
+        'node_modules/admin-lte/bootstrap/js/bootstrap.min.js',
+        'node_modules/admin-lte/plugins/slimScroll/jquery.slimscroll.min.js',
+        'node_modules/admin-lte/dist/js/app.min.js'
     ])
+    .pipe(uglifly())
+    .pipe(concat('deps.min.js'))
+    .pipe(gulp.dest('public/assets/js'))
 })
 
 gulp.task('deps.css', () => {
+    return gulp.src([
+        'node_modules/angular-toastr/dist/angular-toastr.min.css',
+        'node_modules/font-awesome/css/font-awesome.min.css',
+        'node_modules/admin-lte/bootstrap/css/bootstrap.min.css',
+        'node_modules/admin-lte/dist/css/adminLTE.min.css',
+        'node_modules/admin-lte/dist/css/skins/_all-skins.min.css',
+
+    ])
+    .pipe(ugliflycss({ "uglyComments": true}))
+    .pipe(concat('deps.min.css'))
+    .pipe(gulp.dest('public/assets/css'))
     
 })
 gulp.task('deps.fonts', () => {
-
+    return gulp.src([
+        'node_modules/font-awesine/fonts/*.*',
+        'node_modules/admin-lte/bootstrap/fonts/*.*'
+    ])
+    .pipe(gulp.dest('public/assets/fonts'))
 })

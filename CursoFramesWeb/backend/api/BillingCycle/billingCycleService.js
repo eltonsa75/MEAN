99119e8c8ef4638dd5a1eca3225d262f5b021963
eltonsa75 <eltonsa75@hotmail.com>
2024-09-13 08:22:57 -0300

    Criando o SummaryBillingCycleService

--- CursoFramesWeb/backend/api/BillingCycle/billingCycleService.js
+++ CursoFramesWeb/backend/api/BillingCycle/billingCycleService.js
@@ -5,7 +5,7 @@ const BillingCycle = require('./billingCycle')
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 billingCycle.updateOptions({new: true, runValidators: true})
 
-BillingCycleillingCycle.route('count', function(req, res, next ){
+BillingCycle.route('count', function(req, res, next ){
     BillingCycle.count(function(error, value) {
         if(error) {
             res.status(500).json({errors: [error]})

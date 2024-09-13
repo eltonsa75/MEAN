d864e79daa6d5e625bd19658959f961d7885c910
eltonsa75 <eltonsa75@hotmail.com>
2024-09-13 08:57:09 -0300

    Tratando os errors na API

--- CursoFramesWeb/backend/api/billingSummary/billingSummaryService.js
+++ CursoFramesWeb/backend/api/billingSummary/billingSummaryService.js
@@ -1,20 +1,38 @@
-const_ = require('lodash')
+const _ = require('lodash')
 const { sum } = require('lodash')
 const BillingCycle = require('../billingCycle/billingCycle')
 
 // Mais uma função middleware
-function getSummary(req, res){
-    BillingCycle.aggregate({
-        $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}}
-    }, {
-        $group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}}
-    }, {
-        $project: {_id: 0, credit: 1, debt: 1}
-    }, function (error, result) {
-        if(error) {
-            res.status(500).json({errors: [error]})
-        } else {
-            res.json(_.defaults(result[0], {credit: 0, debt: 0}))
+function getSummary(req, res) {
+    BillingCycle.aggregate([
+        {
+            $project: {
+                credit: { $sum: "$credits.value" },
+                debt: { $sum: "$debts.value" }
+            }
+        },
+        {
+            $group: {
+                _id: null,
+                credit: { $sum: "$credit" },
+                debt: { $sum: "$debt" }
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                credit: 1,
+                debt: 1
+            }
         }
-    })
+    ]).exec(function (error, result) {
+        if (error) {
+            res.status(500).json({ errors: [error] });
+        } else {
+            res.json(result.length ? result[0] : { credit: 0, debt: 0 });
+        }
+    });
 }
+
+
+module.exports = { getSummary }
--- CursoFramesWeb/backend/config/routes.js
+++ CursoFramesWeb/backend/config/routes.js
@@ -6,6 +6,11 @@ module.exports = function(server) {
     const router = express.Router()
     server.use('/api', router)
 
+
+    // rotas da API
     const billingCycleService = require('../api/billingCycle/billingCycleService')
     billingCycleService.register(router, '/billingCycles')
+
+    const billingSummaryService = require('../api/billingSummary/billingSummaryService')
+    router.route('/billingSummary').get(billingSummaryService.getSummary)
 }

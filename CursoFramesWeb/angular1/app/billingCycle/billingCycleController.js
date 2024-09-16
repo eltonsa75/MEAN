720c34d5a4c642f663476a062b49dcc7c824e0b9
eltonsa75 <eltonsa75@hotmail.com>
2024-09-16 15:36:01 -0300

    Criando o consumo das Api no Angular

--- CursoFramesWeb/angular1/app/billingCycle/billingCycleController.js
+++ CursoFramesWeb/angular1/app/billingCycle/billingCycleController.js
@@ -2,27 +2,31 @@
     angular.module('primeiraApp').controller('BillingCycleCtrl', [
         '$http',
         'msgs',
+        'tabs',
         BillingCycleController
     ])
 
-    function BillingCycleController($http, msgs) {
+    function BillingCycleController($http, msgs, tabs) {
         const vm = this
         const url = 'http://localhost:3003/api/billingCycles'
 
         vm.refresh = function() {
-            $http.post(url, vm.billingCycle).then(function(response){
+            $http.get(url).then(function (response) {
                 vm.billingCycle = {}
-                vm.billingCycle = response
+                vm.billingCycles = response.data
+                tabs.show(vm, {tabList: true, tabCreate: true})
             })
         }
 
         vm.create = function(){           
-            $http.post(url, vm.billingCycle).then(function(response){
+            $http.post(url, vm.billingCycle).then(function (response) {
+                vm.billingCycle = {}
                 vm.refresh()
                 msgs.addSuccess('Operação realizada com sucesso!')
             }).catch(function(response){
                 msgs.addError(response.data.errors)
             })
         }
+        vm.refresh()
     }
     })()

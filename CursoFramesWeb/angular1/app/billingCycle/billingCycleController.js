9146d6a40ac37ed785fc9045e5c23d70d76e75d9
eltonsa75 <eltonsa75@hotmail.com>
2024-09-19 09:02:01 -0300

    Criando o metodo de Update

--- CursoFramesWeb/angular1/app/billingCycle/billingCycleController.js
+++ CursoFramesWeb/angular1/app/billingCycle/billingCycleController.js
@@ -37,6 +37,16 @@
             tabs.show(vm, {tabDelete: true})
         }
 
+        vm.update = function() {
+            const updateUrl = `${url}/${vm.billingCycle._id}`
+            $http.put(updateUrl, vm.billingCycle).then(function(response){
+                vm.refresh()
+                msgs.addSuccess('Operação realizada com sucesso!')
+            }).error(function(data) {
+                msgs.addError(data.errors)
+            })
+        }
+
         vm.delete = function() {
             const deleteUrl = `${url}/${vm.billingCycle._id}`
             $http.delete(deleteUrl, vm.billingCycle).then(function(response) {

b1c403c87be487fac359ebbebfec56d67ae85485
eltonsa75 <eltonsa75@hotmail.com>
2024-09-19 21:18:53 -0300

    Finalizando o sistema aqui

--- CursoFramesWeb/angular1/app/billingCycle/billingCycleController.js
+++ CursoFramesWeb/angular1/app/billingCycle/billingCycleController.js
@@ -18,24 +18,30 @@
                 vm.billingCycle = {credits: [{}], debts: [{}]}
                 vm.billingCycles = response.data
                 vm.calculateValues()
-                tabs.show(vm, {tabList: true, tabCreate: true})
-
+                
+                // Metodo que calcula a páginação
                 $http.get(`${url}/count`).then(function(response) {
                     vm.pages = Math.ceil(response.value / 10 )
+                    tabs.show(vm, {tabList: true, tabCreate: true})
                     
                 })
             })
         }
 
         vm.create = function(){           
-            $http.post(url, vm.billingCycle).then(function (response) {
+            $http.post(url, vm.billingCycle)
+            .then(function (response) {
                 vm.billingCycle = {}
-                vm.refresh()
-                msgs.addSuccess('Operação realizada com sucesso!')
-            }).catch(function(response){
-                msgs.addError(response.data.errors)
-            })
+                vm.refresh();
+                    msgs.addSuccess('Operação realizada com sucesso!');
+                })
+                .catch(function(error) { // Alterado de .error para .catch
+                    msgs.addError(error.data.errors || 'Ocorreu um erro na operação.')
+                })
         }
+
+
+
         vm.showTabUpdate = function(billingCycle) {
             vm.billingCycle = billingCycle
             vm.calculateValues()

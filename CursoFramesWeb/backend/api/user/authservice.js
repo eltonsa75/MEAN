933be607873b593c66a147c18ed48a3cf4aabff1
eltonsa75 <elton.andrade@refuturiza.com.br>
2024-10-06 11:45:06 -0300

    Metodo de Login

--- CursoFramesWeb/backend/api/user/authservice.js
+++ CursoFramesWeb/backend/api/user/authservice.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const User = require('./user')
 const env = require('../../.env')
+const user = require('./user')
 
 const emailRegex = /\S+@\S+\.\S+/
 const passwordRegex = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,12})/
@@ -11,4 +12,23 @@ const sendErrorsFromDB = (res, dbErrors) => {
     const errors = []
     _.forIn(dbErrors.errors, error => errors.push(error.message))
     return res.status(400).json({errors})
+}
+
+const login = (req, res, next) => {
+    const email = req.body.email || ''
+    const password = req.body.password || ''
+
+    User.find({email}, (err, user) => {
+        if(err) {
+            return sendErrorsFromDB(res, err)
+        } else if (user && bcrypt.compareSync(password, user.password)) {
+            const token = jwt.sign(user, env.authSecret, {
+                expiresIn: "1 day"
+            })
+            const { name, email } = user
+            res.json({ name, email, token })
+        } else {
+            return res.status(400).send({errors: ['Usuário/Senha inválidos']})
+        }
+    })
 }

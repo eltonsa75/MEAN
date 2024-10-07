d7f493b2fe968cb739acc860577edbc1c2b12acd
eltonsa75 <elton.andrade@refuturiza.com.br>
2024-10-07 20:26:32 -0300

    Criando Auth de Login/Cadastro

---
+++ CursoFramesWeb/angular1/app/auth/authController.js
@@ -0,0 +1,30 @@
+(function() {
+
+    angular.module('primeiraApp').controller('AuthCtrl', [
+        '$location',
+        'msgs',
+        AuthController
+    ])
+
+    function AuthController($location, msgs) {
+        const vm = this
+
+        vm.loginMode = true
+
+        vm.changeMode = () => vm.loginMode = !vm.loginMode
+
+        vm.login = () => {
+            console.log(`Login... ${vm.user.email}`)
+        }
+
+        vm.signup = () => {
+            console.log(`Signup... ${vm.user.email}`)
+        }
+
+        vm.getUser = () => ({ name: 'Usuário MOCK', email: 'mock@cod3r.com.br' })
+
+        vm.logout = () => {
+            console.log('Logout...')
+        }
+    }
+})()
